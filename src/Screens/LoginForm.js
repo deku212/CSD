1ee8664263cda694fabe98864f5769c75f83a810
deepakd <deepakd.iprogrammer@gmail.com>
2020-04-14 00:36:02 +0530

    changes

--- src/Screens/LoginForm.js
+++ src/Screens/LoginForm.js
@@ -1,70 +1,78 @@
 import React, {Component} from 'react';
-import {View, Text, StyleSheet, Image, TextInput, TouchableOpacity, KeyboardAvoidingView} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TextInput,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+} from 'react-native';
 
 export default class LoginForm extends Component {
-    render() {
-        return (
-            <KeyboardAvoidingView behavior="padding"style={styles.container}>
-                <TextInput 
-                placeholder="Username or Email"
-                placeholderTextColor='rgba(255,255,255,0.7)'
-                returnKeyType="next"
-                onSubmitEditing={() => this.passwordInput.focus()}
-                keyboardType="email-address"
-                autoCapitalize="none"
-                autoCorrect={false}
-                style={styles.input}
-                />
-                <TextInput 
-                placeholder="Password"
-                placeholderTextColor='rgba(255,255,255,0.7)'
-                returnKeyType="go"
-                secureTextEntry
-                style={styles.input}
-                ref={(input) => this.passwordInput = input}
-                />
-                <View style={styles.buttonContainer}>
-                    <TouchableOpacity style={styles.userButton}>
-                        <Text style={styles.buttonText}>LOGIN</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={styles.userButton}>
-                        <Text style={styles.buttonText}>REGISTER</Text>
-                    </TouchableOpacity>
-                </View>
-            </KeyboardAvoidingView>
-        );
-    }
+  render() {
+    return (
+      <KeyboardAvoidingView behavior="padding" style={styles.container}>
+        <TextInput
+          placeholder="Username or Email"
+          placeholderTextColor="rgba(255,255,255,0.7)"
+          returnKeyType="next"
+          onSubmitEditing={() => this.passwordInput.focus()}
+          keyboardType="email-address"
+          autoCapitalize="none"
+          autoCorrect={false}
+          style={styles.input}
+        />
+        <TextInput
+          placeholder="Password"
+          placeholderTextColor="rgba(255,255,255,0.7)"
+          returnKeyType="go"
+          secureTextEntry
+          style={styles.input}
+          ref={input => (this.passwordInput = input)}
+        />
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.userButton}>
+            <Text style={styles.buttonText}>LOGIN</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.userButton}>
+            <Text style={styles.buttonText}>REGISTER</Text>
+          </TouchableOpacity>
+        </View>
+      </KeyboardAvoidingView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignContent: 'center',
-        justifyContent: 'flex-start'
-    },
-    input: {
-        height: 40,
-        width: 300,
-        backgroundColor: 'rgba(255,255,255,0.3)',
-        marginBottom: 15,
-        color: '#FFF',
-        paddingHorizontal: 20,
-    },
-    buttonContainer: {
-        flexDirection: "row",
-        justifyContent: "space-between",
-        paddingBottom: 30,  
-        width: 300  
-    },
-    userButton: {
-        backgroundColor: '#1B1464',
-        padding: 15,
-        paddingHorizontal: 20,
-        width: 135         
-    },
-    buttonText: {
-        textAlign: 'center',
-        color: '#FFFFFF',
-        fontWeight: '700',
-    }
-}); 
+  container: {
+    flex: 1,
+    alignContent: 'center',
+    justifyContent: 'flex-start',
+  },
+  input: {
+    height: 40,
+    width: 300,
+    backgroundColor: 'rgba(255,255,255,0.3)',
+    marginBottom: 15,
+    color: '#FFF',
+    paddingHorizontal: 20,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingBottom: 30,
+    width: 300,
+  },
+  userButton: {
+    backgroundColor: '#1B1464',
+    padding: 15,
+    paddingHorizontal: 20,
+    width: 135,
+  },
+  buttonText: {
+    textAlign: 'center',
+    color: '#FFFFFF',
+    fontWeight: '700',
+  },
+});
--- src/Screens/LoginScreen.js
+++ src/Screens/LoginScreen.js
@@ -12,35 +12,33 @@ export default class LoginScreen extends Component {
     return (
       <View style={styles.container}>
         <View style={styles.logocontainer}>
-            <Image 
-              style={styles.logo}
-              source={require('./Image/logo.png')} />
+          <Image style={styles.logo} source={require('./Image/logo.png')} />
 
-              <Text style={styles.title}>CSD Demand Store</Text>
+          <Text style={styles.title}>CSD Demand Store</Text>
         </View>
-        <View style={styles.formContainer} >
+        <View style={styles.formContainer}>
+          <LoginForm />
         </View>
-        <LoginForm />
       </View>
     );
   }
 }
 const styles = StyleSheet.create({
   container: {
-    flex :1,
-    backgroundColor : '#3498db',
-    alignItems :'center',
-    justifyContent :'center', 
+    flex: 1,
+    backgroundColor: '#3498db',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   logocontainer: {
     alignItems: 'center',
-    flexGrow: 1,
-    justifyContent: 'center'
+    flex: 1,
+    justifyContent: 'center',
   },
   logo: {
     resizeMode: 'contain',
-    width: 175,
-    height: 175
+    width: 100,
+    height: 100,
   },
   title: {
     color: '#1B1464',
@@ -48,6 +46,9 @@ const styles = StyleSheet.create({
     height: 25,
     textAlign: 'center',
     marginTop: 20,
-    opacity: 0.7
-  }
+    opacity: 0.7,
+  },
+  formContainer: {
+    flex: 1,
+  },
 });
